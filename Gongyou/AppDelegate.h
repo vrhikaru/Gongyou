955dd5947a39b58900fe76b3e86c035441772221
Rex <rexhsu@goodmusic-corp.com>
2017-08-02 11:47:24 +0800

    Initial Commit

---
+++ Gongyou/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Gongyou
+//
+//  Created by Rex on 2017/8/2.
+//  Copyright © 2017年 Rex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Gongyou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gongyou
+//
+//  Created by Rex on 2017/8/2.
+//  Copyright © 2017年 Rex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
